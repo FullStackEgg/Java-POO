248b65ad27c72d4a76204be620abe2bb9a8d2db5
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-08-23 22:20:45 -0300

    Update CineApp.java

--- COLECCIONES EN JAVA/EjercicioColecciones04/CineApp.java
+++ COLECCIONES EN JAVA/EjercicioColecciones04/CineApp.java
@@ -1,3 +1,23 @@
+/**
+* Un cine necesita implementar un sistema en el que se puedan cargar peliculas.
+* Para esto, tendremos una clase Pelicula con el titulo, director y duración de la
+* película (en horas). Implemente las clases y métodos necesarios para esta
+* situación, teniendo en cuenta lo que se pide a continuación:
+*
+* En el main deberemos tener un bucle que crea un objeto Pelicula pidiéndole al
+* usuario todos sus datos y guardándolos en el objeto Pelicula.
+* Después, esa Pelicula se guarda una lista de Peliculas y se le pregunta al usuario
+* si quiere crear otra Pelicula o no.
+* Después de ese bucle realizaremos las siguientes acciones:
+* - Mostrar en pantalla todas las películas.
+* - Mostrar en pantalla todas las películas con una duración mayor a 1 hora.
+* - Ordenar las películas de acuerdo a su duración (de mayor a menor) y mostrarlo en pantalla.
+* - Ordenar las películas de acuerdo a su duración (de menor a mayor) y mostrarlo en pantalla.
+* - Ordenar las películas por titulo, alfabéticamente y mostrarlo en pantalla.
+* - Ordenar las películas por director, alfabéticamente y mostrarlo en pantalla.
+* Nota: recordar el uso del Comparator para ordenar colecciones con objetos.
+*/
+
 package cineApp;
 
 import entidades.Pelicula;
@@ -17,8 +37,7 @@ public class CineApp {
         ArrayList<Pelicula> listaPeliculas = new ArrayList<Pelicula>();
         String opcion;
 
-        //En el main deberemos tener un bucle que crea un objeto Pelicula pidiéndole al
-        //usuario todos sus datos y guardándolos en el objeto Pelicula.
+       // Bucle para crear varias peliculas
         do {
             servicio.CrearPelicula();
 
