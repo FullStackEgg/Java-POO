716847a8f50dcd9ac767ed44785ea283b156f0c5
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-09-08 00:12:41 -0300

    Ejercicio 03 de excepciones

---
+++ MANEJO DE EXCEPCIONES/Ejercicio_03/DivisionNumero.java
@@ -0,0 +1,14 @@
+package ejrcicioIII.entity;
+
+/**
+ * 
+ * @author Felipe Herrera
+ *
+ */
+public class DivisionNumero {
+
+	public static double division(int num1, int num2) throws ArithmeticException {
+		double resultado = num1 / num2;
+		return resultado;
+	}
+}
---
+++ MANEJO DE EXCEPCIONES/Ejercicio_03/EjercicioIII.java
@@ -0,0 +1,49 @@
+/**
+ * Defina una clase llamada DivisionNumero. En el método main utilice un Scanner
+ * para leer dos números en forma de cadena. A continuación, utilice el método
+ * parseInt() de la clase Integer, para convertir las cadenas al tipo int y guardarlas en
+ * dos variables de tipo int. Por ultimo realizar una división con los dos numeros y
+ * mostrar el resultado.
+ * Todas estas operaciones puede tirar excepciones a manejar, el ingreso por
+ * teclado puede causar una excepción de tipo InputMismatchException, el método
+ * Integer.parseInt() si la cadena no puede convertirse a entero, arroja una
+ * NumberFormatException y además, al dividir un número por cero surge una
+ * ArithmeticException. Manipule todas las posibles excepciones utilizando bloques
+ * try/catch para las distintas excepciones
+ */
+package ejrcicioIII;
+
+import java.util.Scanner;
+
+import ejrcicioIII.entity.DivisionNumero;
+
+/**
+ * 
+ * @author Felipe Herrera
+ *
+ */
+public class EjercicioIII {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in).useDelimiter("\n");
+		int num1, num2;
+		
+		try {
+			System.out.println("Ingrese dos numeros para dividir:");
+			System.out.print("\nNum1 = ");
+			num1 = sc.nextInt();
+			
+			System.out.print("\nNum2 = ");
+			num2 = Integer.parseInt(sc.nextLine());
+			
+			DivisionNumero.division(num1, num2);
+			
+		}catch (NumberFormatException e) {
+			System.out.println("Error en el ingreso de datos... " + e);
+		} catch (ArithmeticException e) {
+			System.out.println("Error en la llamada del metodo division... " + e);
+		}
+		
+	}
+}
